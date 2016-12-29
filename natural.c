0a5d020a33309aedee03c13c5890f32491ba8559
NAVANEETHAKRISHNAN01 <navaneethakrishnan.gcse@ksrce.ac.in>
2016-12-29 11:48:20 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,n,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+    sum=sum+i;
+	printf("sum of natural numbers are:%d",sum);
+	getch();
+	
+	
+}
